284c717ceb7fcc88176d0ef75ce767321d6d95dd
ccfortenberry <ccfortenberry@alaska.edu>
2018-02-12 16:33:52 -0900

    Added next case

--- str_calc.cpp
+++ str_calc.cpp
@@ -10,17 +10,29 @@ int str_calc(string input)
 	{
 		return 0;
 	}
+	else if (input.find(",") == 0) 
+	{
+		int result = std::stoi(input, nullptr);
+		return result;
+	}
 	else 
 	{
 		unsigned int i = 0;
-		string buf = "";
-		while(input[i] != ","){
+		string buf1 = "";
+		while(input[i] != ','){
+			buf1 = input.substr(0, i);
 			++i;
-			buf = input.substr(0, i);
 		}
 		
-		int result = std::stoi(input, nullptr);
-		return result;
+		string buf2 = "";
+		while(input[i] != '\0'){
+			buf2 = input.substr(0, i);
+			++i;
+		}
+		
+		int add1 = std::stoi(buf1, nullptr);
+		int add2 = std::stoi(buf2, nullptr);
+		return add1 + add2;
 	}
 }
 
--- test_str_calc.cpp
+++ test_str_calc.cpp
@@ -6,5 +6,6 @@
 TEST_CASE("Empty string returns 0", "[Default]"){
 	REQUIRE(str_calc("")==0);
 	REQUIRE(str_calc("4") == 4);
+	REQUIRE(str_calc("2, 9") == 11);
 }
 
